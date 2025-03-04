403e0da2113154a2ca07e394200280096608aef2
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-04 00:36:34 -0300

    refactor(middleware): corregir lógica de protección de rutas en Clerk
    - Ajustar condición de middleware para proteger solo rutas específicas
    - Modificar expresión regular para capturar subrutas protegidas
    - Invertir condición de autenticación para aplicar protección selectiva

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/protected"]);
+const isProtectedRoute = createRouteMatcher(["/protected(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isProtectedRoute(request)) await auth.protect();
+  if (isProtectedRoute(request)) await auth.protect();
 });
 
 export const config = {
