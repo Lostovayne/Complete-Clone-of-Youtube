3f6c1e51bf54eb3e27122718bb7c2e345abbe34a
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-13 23:09:38 -0300

    feat:(trpc) add prefetching data with an server components for an client component

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -5,9 +5,17 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const ClientPage = () => {
   const trpc = useTRPC();
-  const {data} = useSuspenseQuery(
+  const { data } = useSuspenseQuery(
     trpc.hello.queryOptions({ text: "Epsaind" })
   );
 
-  return <div>{data.greeting}</div>;
+  return (
+    <div>
+      <p>
+        Esto es un client component, haciendo una llamada a una api con
+        prefetching de la data
+      </p>
+      <div>{data.greeting}</div>
+    </div>
+  );
 };
