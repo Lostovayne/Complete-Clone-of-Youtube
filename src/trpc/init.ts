631af85962ca9f997ffa18a568be836f4b19997e
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-16 02:38:27 -0300

    feat: add superjson package

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,10 +1,12 @@
-import { initTRPC } from '@trpc/server';
-import { cache } from 'react';
+import { initTRPC } from "@trpc/server";
+import { cache } from "react";
+import superjson from "superjson";
+
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
-  return { userId: 'user_123' };
+  return { userId: "user_123" };
 });
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
@@ -14,7 +16,7 @@ const t = initTRPC.create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
-  // transformer: superjson,
+  transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
