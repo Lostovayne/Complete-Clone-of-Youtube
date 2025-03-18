987b05a3a867d4ae7abab62ba76b02f5e6ee8c14
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-18 03:06:35 -0300

    feat(trpc): Agregar Error boundary y suspense

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
-// "use client";
-
 import { prefetch, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { ClientPage } from "./client";
 
 export default async function Home() {
   prefetch(trpc.hello.queryOptions({ text: "Epsaind" }));
   // const data = await caller.hello({ text: "Epsaind" });
-  return <ClientPage />;
+  return (
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ClientPage />
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
