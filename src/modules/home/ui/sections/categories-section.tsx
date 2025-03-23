b166367d344b911b60ff083bca512186d44dcb7c
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-23 00:37:05 -0300

    refactor(home): wrap CategoriesSection in Suspense and ErrorBoundary
    Improve user experience by adding loading and error states to the CategoriesSection component. This ensures a smoother interaction when fetching data.

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -2,12 +2,24 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface CategoriesSectionProps {
   categoryId?: string;
 }
 
 export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Error...</div>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
 
