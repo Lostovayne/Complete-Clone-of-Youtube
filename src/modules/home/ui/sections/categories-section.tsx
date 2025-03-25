5bac7cc6db1e4929ae2156954c3ef2ee48ac053c
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-25 02:25:45 -0300

    refactor(home): improve categories section and filter carousel
    - Add loading skeleton for categories section
    - Implement navigation logic for category selection
    - Enhance filter carousel with fade effects and loading states

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -2,6 +2,7 @@
 import { FilterCarousel } from "@/components/filter-carousel";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -11,7 +12,7 @@ interface CategoriesSectionProps {
 
 export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<CategoriesSkeleton />}>
       <ErrorBoundary fallback={<div>Error...</div>}>
         <CategoriesSectionSuspense categoryId={categoryId} />
       </ErrorBoundary>
@@ -19,12 +20,29 @@ export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
   );
 };
 
+const CategoriesSkeleton = () => {
+  return <FilterCarousel isLoading data={[]} onSelect={() => {}} />;
+};
+
 export const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
   const trpc = useTRPC();
+  const router = useRouter();
+
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
   const categories = data.map((category) => ({
     value: category.id,
     label: category.name,
   }));
-  return <FilterCarousel data={categories} value={categoryId} />;
+
+  const onSelect = (value: string | null) => {
+    const url = new URL(window.location.href);
+    if (value) {
+      url.searchParams.set("categoryId", value);
+      // window.history.pushState({}, "", url.toString());
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+    router.push(url.toString());
+  };
+  return <FilterCarousel onSelect={onSelect} data={categories} value={categoryId} />;
 };
