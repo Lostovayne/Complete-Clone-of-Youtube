c761b86224c7c3ed23eeb552a0a8d4d81d87a516
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-29 01:55:51 -0300

    style(studio): add border and shadow to navbar and update sidebar styling
    Improve visual consistency by adding a border and shadow to the studio navbar.
    Refactor the sidebar to include a direct exit button with an icon, enhancing user navigation and experience.

--- src/modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,14 +1,22 @@
 import { Separator } from "@/components/ui/separator";
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { LogOutIcon } from "lucide-react";
+import Link from "next/link";
 import { MainSection } from "./main-section";
-import { PersonalSection } from "./personal-section";
 export const StudioSidebar = () => {
   return (
-    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+    <Sidebar className="pt-16 z-40" collapsible="icon">
       <SidebarContent className="bg-background">
         <MainSection />
         <Separator />
-        <PersonalSection />
+        <SidebarMenuItem>
+          <SidebarMenuButton>
+            <Link href={"/"} prefetch>
+              <LogOutIcon className="size-5" />
+              <span className="text-sm">Exit studio</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
       </SidebarContent>
     </Sidebar>
   );
