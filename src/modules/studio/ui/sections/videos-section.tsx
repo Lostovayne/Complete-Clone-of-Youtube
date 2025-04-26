1efb7d2df3cc7061ef0fd9c7a3dc0dd20215d494
LostoVayne <moises.barrera.dev@gmail.com>
2025-04-26 17:19:55 -0400

    feat(studio): add table component for videos section
    Introduce a new table component to display video data in the studio section. This improves the user interface by organizing video information in a structured and readable format. The table includes columns for video title, visibility, status, date, views, comments, and likes, with each row being clickable to navigate to the video details page.

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -6,6 +6,16 @@ import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+
 export const VideosSection = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -18,21 +28,56 @@ export const VideosSection = () => {
 
 export const VideosSectionSuspense = () => {
   const trpc = useTRPC();
-  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
-    useSuspenseInfiniteQuery(
-      trpc.studio.getMany.infiniteQueryOptions(
-        { limit: DEFAULT_LIMIT },
-        {
-          getNextPageParam(lastPage) {
-            return lastPage.nextCursor;
-          },
-        }
-      )
-    );
+  const {
+    data: videos,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useSuspenseInfiniteQuery(
+    trpc.studio.getMany.infiniteQueryOptions(
+      { limit: DEFAULT_LIMIT },
+      {
+        getNextPageParam(lastPage) {
+          return lastPage.nextCursor;
+        },
+      }
+    )
+  );
 
   return (
     <div>
-      {JSON.stringify(data)}
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="">Views</TableHead>
+              <TableHead className="">Comments</TableHead>
+              <TableHead className="">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link href={`/studio/videos/${video.id}`} key={video.id} prefetch legacyBehavior >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>Visibility</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Views</TableCell>
+                    <TableCell>Comments</TableCell>
+                    <TableCell>Likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
       <InfiniteScroll
         isManual
         hasNextPage={hasNextPage}
