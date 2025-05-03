54b53d4056352fbcda7082795bf1a6621a6e4949
LostoVayne <moises.barrera.dev@gmail.com>
2025-05-03 01:49:09 -0400

    feat: add responsive dialog and drawer components
    Add a new `ResponsiveModal` component that switches between `Dialog` and `Drawer` based on the device type. Also, introduce a new `Drawer` component with its sub-components for mobile-friendly interactions. Update dependencies to their latest versions for improved functionality and security.

--- src/hooks/use-intersection-observer.ts
+++ src/hooks/use-intersection-observer.ts
@@ -1,8 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 
-export const useIntersectionObserver = <T extends Element>(
-  options?: IntersectionObserverInit
-) => {
+  /**
+   * @description
+   * React hook that uses the Intersection Observer API to observe whether
+   * an element is intersecting with the viewport or not.
+   *
+   * @param {IntersectionObserverInit} [options] - Options for the `IntersectionObserver` constructor.
+   * @returns {object} - An object with two properties: `targetRef` and `isIntersecting`.
+   *   `targetRef` is a React ref that should be assigned to the element that
+   *   you want to observe. `isIntersecting` is a boolean that is `true` if
+   *   the element is intersecting with the viewport, and `false` otherwise.
+   */
+export const useIntersectionObserver = <T extends Element>(options?: IntersectionObserverInit) => {
   const [isIntersecting, setIsIntersecting] = useState<boolean>(false);
   const targetRef = useRef<T | null>(null);
 
