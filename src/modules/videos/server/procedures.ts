7218049c724435aacb14542f74b9ae8c32638d49
LostoVayne <moises.barrera.dev@gmail.com>
2025-05-07 03:39:31 -0400

    feat(mux): agregar nuevos campos y lógica para la carga de videos
    Se han añadido nuevos campos relacionados con Mux en el esquema de videos, incluyendo `muxStatus`, `muxAssetId`, `muxUploadId`, `muxPlaybackId`, `muxTrackId` y `muxTrackStatus`. Además, se ha actualizado el `videosRouter` para establecer el estado de carga y el ID de carga al crear un nuevo video.

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -29,6 +29,8 @@ export const videosRouter = createTRPCRouter({
       .values({
         userId,
         title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
 
